1261995a4ce00d6021d7393392c6a61fd36df486
spike <weisuojiaoshou@gmail.com>
2017-03-19 21:27:59 +0900

    Implement basic login

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase) 
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      redirect_to user
+    # 登入用户,然后重定向到用户的资料页面 
+    else
+    flash.now[:danger] = 'Invalid email/password combination'
+    render 'new'
+    end
+  end
+
+  def destroy 
+    log_out
+    redirect_to root_url
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  get 'sessions/new'
+
   get 'users/new'
 
   root 'static_pages#home'
@@ -13,6 +15,11 @@ Rails.application.routes.draw do
   get '/contact', to: 'static_pages#contact'
 
   get '/signup', to: 'users#new'
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create' 
+  delete '/logout', to: 'sessions#destroy'
+
 
   resources :users
 end
