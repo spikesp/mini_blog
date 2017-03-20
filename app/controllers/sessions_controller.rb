0b36961446d50447cf6abe96f4a56a99e6ad5bef
spike <weisuojiaoshou@gmail.com>
2017-03-20 11:42:50 +0900

    Finish user edit, update, index, and destroy actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       log_in user
       params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-      redirect_to user
+      redirect_back_or user
     # 登入用户,然后重定向到用户的资料页面 
     else
     flash.now[:danger] = 'Invalid email/password combination'
