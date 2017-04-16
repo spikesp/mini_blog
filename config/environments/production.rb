7109b26bc19e67e0574ef2f71a19368fbf6bd6f5
spike <weisuojiaoshou@gmail.com>
2017-04-16 21:26:24 +0900

    fix the host name issue in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,7 +25,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = '<your heroku app>.herokuapp.com'
+  host = 'fierce-bastion-28465.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
