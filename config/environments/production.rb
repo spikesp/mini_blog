d5188589b579ab3d5d3324bef31175dfb62b9356
spike <weisuojiaoshou@gmail.com>
2017-04-16 20:01:37 +0900

    Add acount activation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,6 +22,21 @@ Rails.application.configure do
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
+
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  host = '<your heroku app>.herokuapp.com'
+  config.action_mailer.default_url_options = { host: host }
+  ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
+  
+  }
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
